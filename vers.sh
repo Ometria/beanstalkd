fa4c5931bb36e7dbb5258e1a731a7507c9fe2c28
Simeon Visser <simeon@ometria.com>
2014-08-11 12:05:27 +0100

    Modified version script to ensure beanstalkd -v always reflects that this is our modified version

--- vers.sh
+++ vers.sh
@@ -4,7 +4,7 @@ if git describe >/dev/null 2>&1
 then
     git describe --tags --match=dev* | sed s/^dev// | tr - + | tr -d '\n'
     if ! git diff --quiet HEAD
-    then printf +mod
+    then printf +mod+ometria
     fi
 else
     printf unknown
