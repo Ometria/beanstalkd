54b98ba7a28808607621c666adf0137088d0ee5c
Simeon Visser <simeon@ometria.com>
2014-08-11 12:11:32 +0100

    Make sure we always append +ometria regardless of changes (or not) to HEAD

--- vers.sh
+++ vers.sh
@@ -4,8 +4,9 @@ if git describe >/dev/null 2>&1
 then
     git describe --tags --match=dev* | sed s/^dev// | tr - + | tr -d '\n'
     if ! git diff --quiet HEAD
-    then printf +mod+ometria
+    then printf +mod
     fi
+    printf +ometria
 else
     printf unknown
 fi
